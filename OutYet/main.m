f0349437f360415711322e0354074d1a0378f022
mrtnlst <martinlist@me.com>
2017-08-30 20:44:41 +0200

    Initial Commit

---
+++ OutYet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OutYet
+//
+//  Created by martin on 30.08.17.
+//  Copyright © 2017 martinlist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
